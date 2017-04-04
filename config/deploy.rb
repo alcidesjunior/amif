93802d8a4a3755c722513cfe69644ceba3ec279d
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-04-03 22:00:56 -0300

    setted rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :application,     'amif'
 set :user,            'alcides'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :rbenv_ruby, '2.0.0-p247'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
