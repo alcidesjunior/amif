e8a4f16663a6e77336371db9d2ae5c97ad0e89ef
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-04-06 19:02:11 -0300

    server

--- app/controllers/Users/confirmations_controller.rb
+++ app/controllers/Users/confirmations_controller.rb
@@ -1,4 +1,5 @@
-class Users::ConfirmationsController < Devise::ConfirmationsController
+class User::ConfirmationsController < Devise::ConfirmationsController
+
   # GET /resource/confirmation/new
   # def new
   #   super
