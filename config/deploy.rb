db76c16473b6ee376f63319487ce2935515f1162
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-04-27 12:27:29 -0300

    ;

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :pty, true
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets/' ,'public/uploads', 'public/profile')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets' ,'public/uploads', 'public/profile')
 
 set :config_example_suffix, '.example'
 set :config_files, %w{config/database.yml config/secrets.yml}
