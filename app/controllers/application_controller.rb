004c4b0bbcd4987ee5a73cacb4a0ac03b6db91ad
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-06-05 12:27:56 -0300

    pagina raiz alterada para ser o blog e ao realizar o login o usuario eh redirecionado para a pagina home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  private
+
+ # Overwriting the sign_out redirect path method
+ def after_sign_in_path_for(resource_or_scope)
+   home_path
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,16 @@ Rails.application.routes.draw do
   resources :modalidades
   resources :professors
   resources :atleta
-  devise_for :users
+  # devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+    # root "home#index"
+    get "amif" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+  get '/home', to: 'home#index'
   get 'profile/:id', to: 'admins#edit'
   post '/saveFrontPanel/:id', to: 'home#saveFrontPanel'
   get '/blog', to: 'home#blog'
